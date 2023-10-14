c93d6e3af39cc3a616a37e7fd196145506d33f5e
Deepak R <deepakr948267@gmail.com>
2023-10-14 18:17:28 +0530

    git cache cleared

--- price.py
+++ price.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
 from twilio.rest import Client
 from email.mime.multipart import MIMEMultipart
@@ -26,23 +26,16 @@ class MyTask:
         server.quit()
     def get_HTML(self):
         try:
-            chromedriver_path =r"E:\Downloads\chromedriver-win64 (1)\chromedriver-win64\chromedriver.exe"
-            options = webdriver.ChromeOptions()
-            options.add_argument('--headless')
-            driver = webdriver.Chrome(executable_path=chromedriver_path, options=options)
-            url = 'https://www.krishimaratavahini.kar.nic.in/MainPage/DailyMrktPriceRep2.aspx?Rep=Com&CommCode=140&VarCode=1&Date=12/10/2018&CommName=Arecanut%20/%20%E0%B2%85%E0%B2%A1%E0%B2%BF%E0%B2%95%E0%B3%86&VarName=Red%20/%20%E0%B2%95%E0%B3%86%E0%B2%82%E0%B2%AA%E0%B3%81'
-            driver.get(url)
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-
+            import requests
+            url = "https://www.krishimaratavahini.kar.nic.in/MainPage/DailyMrktPriceRep2.aspx?Rep=Com&CommCode=140&VarCode=1&Date=12/10/2018&CommName=Arecanut%20/%20%E0%B2%85%E0%B2%A1%E0%B2%BF%E0%B2%95%E0%B3%86&VarName=Red%20/%20%E0%B2%95%E0%B3%86%E0%B2%82%E0%B2%AA%E0%B3%81"
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, "html.parser")
             table = soup.find('table', id='_ctl0_content5_Table1')
-
             rows = table.find_all('tr')
             self.get_data(rows)
         except Exception as error:
             print(error)
             self.error_send(error)
-
-
 
     def get_data(self, rows):
         try:
