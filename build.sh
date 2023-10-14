256c77cdeabb658c90a94959725e1d54f96b2e30
Deepak R <deepakr948267@gmail.com>
2023-10-14 12:29:05 +0530

    added a build.sh file

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Upgrade pip
+/usr/local/bin/python3 -m pip install --upgrade pip
+
+# Install packages from requirements.txt
+/usr/local/bin/python3 -m pip install -r requirements.txt
