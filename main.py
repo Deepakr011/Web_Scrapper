cca515392e94b464845b35b588ed56b3dbdd9a1c
Deepakr011 <137927037+Deepakr011@users.noreply.github.com>
2023-10-15 13:19:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from price import MyTask
+from apscheduler.triggers.cron import CronTrigger
 from flask import Flask
+from Areca_price_update import MyTask
 import threading
+
+# Create a Flask app for the HTTP server
+app = Flask(__name__)
+
+# Define a function to run the task
+def send_message_job():
+    my_task.get_HTML()
 
 # Create an instance of the MyTask class
 my_task = MyTask()
@@ -9,26 +17,18 @@ my_task = MyTask()
 # Create a scheduler
 scheduler = BackgroundScheduler()
 
-# Define a function to run the task
-def send_message_job():
-    my_task.get_HTML()
+trigger = CronTrigger(second=0, minute=0, hour=9)
+scheduler.add_job(send_message_job, trigger=trigger, misfire_grace_time=600)
 
-# Schedule the job to run daily at 8 am
-scheduler.add_job(send_message_job, 'cron', hour=8, minute=0)
 
-# Start the scheduler in a separate thread
 scheduler_thread = threading.Thread(target=scheduler.start)
 scheduler_thread.daemon = True
 scheduler_thread.start()
-
-# Create a simple Flask app for the HTTP server
-app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello, this is your Flask server!"
 
 if __name__ == '__main__':
-    # Start the Flask app after the scheduler is initialized
     app.run(host='0.0.0.0', port=5000)
     print("Flask server started successfully!")
